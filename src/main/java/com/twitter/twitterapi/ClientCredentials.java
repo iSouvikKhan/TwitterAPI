1a45520802a89f5379694a4f422a029c83591fd0
Souvik Khan <souvik97381@gmail.com>
2024-04-11 06:13:30 +0530

    initial

---
+++ src/main/java/com/twitter/twitterapi/ClientCredentials.java
@@ -0,0 +1,5 @@
+package com.twitter.twitterapi;
+
+public class ClientCredentials {
+
+}
---
+++ src/main/java/com/twitter/twitterapi/TwitterConfig.java
@@ -0,0 +1,10 @@
+package com.twitter.twitterapi;
+
+public class TwitterConfig {
+
+	public static final String CONSUMER_KEY = "...";
+    public static final String CONSUMER_SECRET = "...";
+    public static final String ACCESS_TOKEN = "...";
+    public static final String ACCESS_TOKEN_SECRET = "...";
+    
+}
